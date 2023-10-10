83745c293808a33c91a34eb7fc52cd0101a7251e
lokesh-wagh <lokeshwagh004@gmail.com>
2023-10-10 23:47:58 +0530

    optimized chatbot to suggest spotify

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
 const chatbotRoutes = require("./routes/chatbotRoutes");
 const memoryGameRoutes = require("./routes/memoryGameRoutes");
 const animatedRoutes = require("./routes/animatedRoutes");
+const fetch=require('node-fetch-commonjs')
 // require("dotenv").config({ path: "" });
 const PORT = process.env.PORT || process.env.API_PORT;
 
@@ -57,7 +58,7 @@ app.use(function (req, res, next) {
 
 app.post("/send-msg", (req, res) => {
   runSample(req.body.MSG).then((data) => {
-    res.send({ Reply: data });
+    res.send({ Reply: data.reply ,Intent: data.intent,uri:data.uri});
   });
 });
 
@@ -65,13 +66,13 @@ app.post("/send-msg", (req, res) => {
  * Send a query to the dialogflow agent, and return the query result.
  * @param {string} projectId The project to be used
  */
-async function runSample(msg, projectId = "test-chat-bot-app-381318") {
+async function runSample(msg, projectId = "database-c051f") {
   // A unique identifier for the given session
 
   // Create a new session
   const sessionClient = new dialogflow.SessionsClient({
     keyFilename:
-      "C:/Users/Silikie/OneDrive/Desktop/hack36/hh633/backend/test-chat-bot-app-381318-7f9bf23507a6.json",
+      "C:/Users/lokes/Desktop/play/PlayTopia/backend/database-c051f-5b8ec5e81639.json",
   });
   const sessionPath = sessionClient.projectAgentSessionPath(
     projectId,
@@ -102,7 +103,64 @@ async function runSample(msg, projectId = "test-chat-bot-app-381318") {
   } else {
     console.log("  No intent matched.");
   }
-  return result.fulfillmentText;
+  
+  //if the intent is to suggest song or search song then we make api call to generate the result
+  if(result.intent.displayName=="suggest song's"){
+    const uri= await generateURIS(result.parameters.fields.genre?.stringValue+result.parameters.fields.musicartist?.stringValue); //generate endpoint
+    return {reply:result.fulfillmentText,intent:result.intent.displayName,uri:uri}
+ }
+ if(result.intent.displayName=="search songs"){
+   const uri=await generateURIS(result.queryText); //generate endpoint
+   return {reply:result.fulfillmentText,intent:result.intent.displayName,uri:uri}
+ }
+  return {reply:result.fulfillmentText,intent:result.intent,uri:[]};
+}
+
+
+async function generateURIS(query){
+  try{
+    if(query==""){
+      query="trending" //this give's what will be shown if bot fail's to display anything
+    }
+   
+    const params1={ //credential's for spotify api for generating token
+      client_secret:'906dd71f26fd49a89f98e8e6beff0ed3',
+      client_id:'0e4f1a2f3ddc4b61a52631b1db690ee7',
+      grant_type:'client_credentials',
+    }
+    const url1 = new URL('https://accounts.spotify.com/api/token'); //token generation 
+    url1.search = new URLSearchParams(params1).toString();
+
+    const options1={
+      method: 'POST',
+      headers:{'Content-Type':'application/x-www-form-urlencoded'}
+    }
+    const res1=await fetch(url1,options1)
+    const data1=await res1.json();
+    
+    const url2=new URL('https://api.spotify.com/v1/search'); //actual query to the api to get the uri
+    const params2={
+      q:query,
+      type:'track',
+      offset:'0',
+      limit:'3', //change this to adjust the query number
+    }
+    url2.search = new URLSearchParams(params2).toString();
+    const options2={
+      method:'GET',
+      headers:{'Authorization':data1.token_type+' '+data1.access_token}
+    }
+    const res2=await fetch(url2,options2);
+    const data2=await res2.json();
+    
+    const uriIds=data2.tracks.items.map((item)=>{return item.id}); //filtering the data to have relevant fields only
+   
+    return uriIds; //return a array of relevant fields
+  }
+  catch(e){
+    console.log(e);
+  }
+  
 }
 
 // app.get("/chatbot", function (req, res) {
